505b7bd11b7c2ae5ff9b34be726a7c4cd7cf94c7
sharpiestealr <62366339+sharpiestealr@users.noreply.github.com>
2020-04-03 16:15:35 -0300

    commit like 6? 7?
    ! separated point from plb (mistake from before)
    ! finished commenting on the methods for clarity
    
    am more and more convinced i'll be running this all through an enum

--- craps.py
+++ craps.py
@@ -35,12 +35,16 @@ def rewind(wl):
     if (wl == 0):
         reset = input("Você perdeu o jogo. Você gostaria de jogar novamente? (sim/não)")
         if (fichas == 0):
+            #resets game in case of loss
+            #set as an if because if scoop, it's possible to cary on with your own chips as in real cassino
             fichas = 10
     elif (wl == 1):
         reset = input("Parabéns, você venceu o jogo com ", {1}, " fichas! Você gostaria de jogar novamente? (sim/não)", fichas)
+        #winning doesn't reset chips like a cassino as well
     if (reset == "sim"):
         wl = 2
         phase = 0
+        print("Você possui ", {1}, " fichas.", fichas)
     elif (reset == "não" or "nao"):
         sys.exit(0)
     return phase
@@ -49,8 +53,9 @@ def plb (fichas, sumD):
     #pass line bet
     if (phase != 0):
         #guarantee of only during come out
-        return null
+        return phase
     else:
+        print("O jogo está na fase ", {1}, ".", phases(phase))
         bet(fichas)
         if (sumD == 7 or 11):
             fichas = fichas + bet
@@ -62,17 +67,22 @@ def plb (fichas, sumD):
             phase = 0
         elif (sumD == 4 or 5 or 6 or 8 or 9 or 10):
             phase = 1
-            print("O jogo está na fase ", {1}, ".", phases(phase))
-            pointS = sumD
-            rolldice(d1,d2)
-            if (sumD == pointS):
-                fichas = fichas + bet
-                wl = 1
-            elif (sumD == 7):
-                fichas = fichas - bet
-                wl = 0
-            else:
-                rolldice(d1, d2)
+            #this controls the change of phase into point
     return phase
+
+def point(bet, sumD):
+    #method that organizes point phase
+    print("O jogo está na fase ", {1}, ".", phases(phase))
+    pointS = sumD
+    rolldice(d1,d2)
+    if (sumD == pointS):
+        fichas = fichas + bet
+        wl = 1
+    elif (sumD == 7):
+        fichas = fichas - bet
+        wl = 0
+    else:
+        rolldice(d1, d2)
+    return wl
 
 
