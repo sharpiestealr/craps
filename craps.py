6506241f25737d8d873c5eeb1a0880f1964b0ca1
sharpiestealr <62366339+sharpiestealr@users.noreply.github.com>
2020-04-03 16:21:45 -0300

    gambling is illegal
    in this new update there is no more betting phase for obvious reasons that it's not necessary

--- craps.py
+++ craps.py
@@ -11,7 +11,6 @@ reset = " "
 class phases(Enum):
     COME_OUT = 0
     POINT = 1
-    APOSTAS = 2
 
 def rolldice (d1, d2):
     #rola 2 dados random e soma
@@ -22,7 +21,6 @@ def rolldice (d1, d2):
 
 def bet(fichas):
     #automated betting system
-    phase = 2
     while (bet > fichas):
         bet = int(input("Digite quantas fichas você gostaria de apostar, por favor."))
         if (bet > fichas):
@@ -30,7 +28,7 @@ def bet(fichas):
             print("Aposta inválida. Por favor, aposte uma quantidade de fichas que você possua.")
     return bet
 
-def rewind(wl):
+def rewind(wl, fichas, phase):
     #resets game
     if (wl == 0):
         reset = input("Você perdeu o jogo. Você gostaria de jogar novamente? (sim/não)")
@@ -49,14 +47,14 @@ def rewind(wl):
         sys.exit(0)
     return phase
 
-def plb (fichas, sumD):
+def plb (phase, bet, fichas, sumD, wl):
     #pass line bet
     if (phase != 0):
         #guarantee of only during come out
         return phase
     else:
         print("O jogo está na fase ", {1}, ".", phases(phase))
-        bet(fichas)
+        bet = bet(fichas)
         if (sumD == 7 or 11):
             fichas = fichas + bet
             wl = 2
@@ -70,7 +68,7 @@ def plb (fichas, sumD):
             #this controls the change of phase into point
     return phase
 
-def point(bet, sumD):
+def point(phase, sumD, fichas, bet):
     #method that organizes point phase
     print("O jogo está na fase ", {1}, ".", phases(phase))
     pointS = sumD
@@ -84,5 +82,4 @@ def point(bet, sumD):
     else:
         rolldice(d1, d2)
     return wl
-
 
