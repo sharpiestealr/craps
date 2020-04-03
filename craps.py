6c11d957d591632b828ef2b6dd4c45b423fd73e8
sharpiestealr <62366339+sharpiestealr@users.noreply.github.com>
2020-04-03 14:24:59 -0300

    ok it's getting real
    + new file for separating instructions commentary in new tab
    + 3 methods

--- craps.py
+++ craps.py
@@ -1,52 +1,27 @@
 
-#2 fases: come out, point
-#SEMPRE informar qual fase tá
-#computador lança 2 d6 secretos
-    #objetivo é adivinhar o resultado?
-#jogador escolhe se aposta ou sai
-    #automaticamente removido se tiver sem ficha
-#SEMPRE informar o jogador sobre o gamestate
-    #mostrar possibilidades de apostas e valores
-    #podem ser feitas apostas simultâneas de valores e tipos diferentes
-
-#tipos de apostas
-    #pass line bet
-        #SÓ NO COME OUT
-        #7 ou 11: win (double fichas apostadas)
-        #2, 3 ou 12 (CRAPS): loss (perde apostadas e recebe nada)
-        #4, 5, 6, 8, 9 ou 10: jogo vai pra POINT
-            #em POINT: aposta mantém 
-                #soma dos dados vira point
-                #relança os d6
-                    #se d6 == point, win
-                    #se d6 == 7, loss
-                    #else, rejoga até conseguir point ou sair 7
-        #no fim de plb, acaba esta rodada e começa COME OUT nova
-    #field
-        #em qualquer fase
-        #5, 6, 7 ou 8: perde apostadas e recebe nada
-        #3, 4, 9, 10 ou 11: neutro (recebe de volta o que apostou, delta 0)
-        #2: dobra as fichas apostadas
-        #12: triplica as fichas apostadas
-    #any craps
-        #em qualquer fase
-        #2, 3 ou 12: x7 apostadas
-        #else, perde aposta
-    #twelve
-        #em qualquer fase
-        #12: x30 apostadas
-        #else, perde aposta
-
-#will need:
-    #var: d1, d2, fichas, sumD, pointS, wl, phase
-        #wl como controle de rodadas
-        #phase como controle de come out vs point
-    #methods:
-        #rolldice: rola 2 dados random e soma - return sumD
-        #plb: testa as condições - retorna phase
-            #point: testa as condições - retorna wl
-        #field: testa as condições - retorna phase
-        #anyC: testa as condições - retorna phase
-        #twelve: testa as condições - retorna phase
-
 def rolldice (d1, d2):
+    d1 = random.range[1, 6, 1]
+    d2 = random.range[1, 6, 1]
+    sumD = d1 + d2
+    return sumD
+
+def bet(fichas):
+    phase = 2
+    bet = int(input("Quanto você gostaria de apostar?"))
+    if (bet > fichas):
+        print("Aposta inválida. Por favor, aposte uma quantidade de fichas que você possua.")
+        phase = 3
+    return bet
+
+def plb (fichas, sumD):
+    if (phase != 0):
+        return null
+    else:
+        bet(fichas)
+
+    return phase
+
+fichas, bet = 10
+d1, d2 = 0
+wl, phase = 2
+
---
+++ instructions.py
@@ -0,0 +1,57 @@
+#2 fases: come out, point
+#SEMPRE informar qual fase tá
+#computador lança 2 d6 secretos
+    #objetivo é adivinhar o resultado?
+#jogador escolhe se aposta ou sai
+    #automaticamente removido se tiver sem ficha
+#SEMPRE informar o jogador sobre o gamestate
+    #mostrar possibilidades de apostas e valores
+    #podem ser feitas apostas simultâneas de valores e tipos diferentes
+
+#tipos de apostas
+    #pass line bet
+        #SÓ NO COME OUT
+        #7 ou 11: win (double fichas apostadas)
+        #2, 3 ou 12 (CRAPS): loss (perde apostadas e recebe nada)
+        #4, 5, 6, 8, 9 ou 10: jogo vai pra POINT
+            #em POINT: aposta mantém 
+                #soma dos dados vira point
+                #relança os d6
+                    #se d6 == point, win
+                    #se d6 == 7, loss
+                    #else, rejoga até conseguir point ou sair 7
+        #no fim de plb, acaba esta rodada e começa COME OUT nova
+    #field
+        #em qualquer fase
+        #5, 6, 7 ou 8: perde apostadas e recebe nada
+        #3, 4, 9, 10 ou 11: neutro (recebe de volta o que apostou, delta 0)
+        #2: dobra as fichas apostadas
+        #12: triplica as fichas apostadas
+    #any craps
+        #em qualquer fase
+        #2, 3 ou 12: x7 apostadas
+        #else, perde aposta
+    #twelve
+        #em qualquer fase
+        #12: x30 apostadas
+        #else, perde aposta
+
+#will need:
+    #var: d1, d2, fichas, sumD, pointS, wl, phase, bet
+        #wl como controle de rodadas
+            # 0 loss
+            # 1 win
+            # 2 game in progress
+        #phase como controle de come out vs point
+            # 0 come out
+            # 1 point
+            # 2 in progress
+            # 3 betting
+    #methods:
+        #rolldice: rola 2 dados random e soma - return sumD
+        #plb: testa as condições - retorna phase
+            #point: testa as condições - retorna wl
+        #field: testa as condições - retorna phase
+        #anyC: testa as condições - retorna phase
+        #twelve: testa as condições - retorna phase
+        #bet: automated betting system - return bet
